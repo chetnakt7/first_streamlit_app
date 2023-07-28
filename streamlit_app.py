0da05dd33021d5df5308c4849520a28baea89881
chetnakt7 <140746918+chetnakt7@users.noreply.github.com>
2023-07-28 10:50:36 +0530

    Update streamlit_app.py
    error correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-streamlit.title('My Mom's new healthy diner')
+streamlit.title("My Mom's new healthy diner")
 streamlit.header('Breakfast Favourite')
 streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
